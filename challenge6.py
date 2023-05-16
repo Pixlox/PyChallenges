1fa71c69ec5799617c1c61fcb4ea8292b9174d41
Omar H <85137818+Pixlox@users.noreply.github.com>
2023-05-16 18:31:51 +1000

    Add challenge 6.

---
+++ challenge6.py
@@ -0,0 +1,14 @@
+from graphics import *
+import random
+
+def main():
+    win = GraphWin("random circles", 500, 500)
+
+    center_x, center_y = win.getWidth() / 2, win.getHeight() / 2
+    
+    c = Circle(Point(center_x, center_y), random.randint(30, 200))
+    
+    c.draw(win)
+    win.getMouse()
+    win.Close()
+main()
