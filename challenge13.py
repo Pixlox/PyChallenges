1d6023b5dcaed2d9f9340b71b4f305fdeb97c285
Omar H <85137818+Pixlox@users.noreply.github.com>
2023-05-16 21:12:48 +1000

    added challenge 13

---
+++ challenge13.py
@@ -0,0 +1,23 @@
+from graphics import *
+import time
+import random
+
+def main():
+    win = GraphWin("sleep timer shape", 400, 400)
+
+    shape = Circle(Point(200, 200), 50)
+    shape.setFill("red")
+    shape.draw(win)
+
+    while True:
+        new_x = random.randint(0, win.getWidth())
+        new_y = random.randint(0, win.getHeight())
+        dx = new_x - shape.getCenter().getX()
+        dy = new_y - shape.getCenter().getY()
+        shape.move(dx, dy)
+
+        time.sleep(0.5)
+
+    win.close()
+
+main()
