493b38509498ac8a66665401fd7598ad13c8a997
Omar H <85137818+Pixlox@users.noreply.github.com>
2023-05-22 14:21:53 +1000

    fix to deathly hallows code, moved around a few things

--- challenge3b.py
+++ challenge3b.py
@@ -5,9 +5,6 @@ def main():
 
     center_x, center_y = win.getWidth() / 2, win.getHeight() / 2
 
-    message = Text(Point(center_x, center_y + 150), "colour wow")
-    message.draw(win)
-
     triangle_size = 200
     triangle_height = triangle_size * (3 ** 0.5) / 2
 
@@ -21,20 +18,20 @@ def main():
     
     triangle.draw(win)
 
-    circle_radius = triangle_size / 4
-    circle_center = Point(center_x, center_y + 35 + triangle_height / 6)
+    circle_radius = triangle_size / 3.6
+    circle_center = Point(center_x, center_y + 32 + triangle_height / 6)
 
     circle = Circle(circle_center, circle_radius)
     circle.setWidth(2)
     circle.setOutline("red")
     circle.draw(win)
 
-    line_start = Point(center_x, center_y + 35 - triangle_height / 2)
-    line_end = Point(center_x, center_y + 35 + triangle_height / 2)
+    line_start = Point(center_x, center_y + 32 - triangle_height / 2)
+    line_end = Point(center_x, center_y + 30 + triangle_height / 2)
 
     line = Line(line_start, line_end)
     line.setWidth(2)
-    line.setOutline("yellow")
+    line.setOutline("green")
     line.draw(win)
 
     win.mainloop()
