f5b6852984b03b40da685d564ca52bd348f9b0f0
Pixlox <85137818+Pixlox@users.noreply.github.com>
2023-05-25 20:15:00 +1000

    added detection if invalid answer

--- hi-lo-game.py
+++ hi-lo-game.py
@@ -9,10 +9,14 @@ def grabDifficulty():
         return 20
     elif difficultyWord == "hard":
         return 30
+    else:
+        print("Invalid answer.")
+        print("Defaulting to difficulty - easy")
+        return 10
 
 difficulty = grabDifficulty()
 secret = random.randint(1, difficulty)
-count = 0
+count = 1
 
 name = input("What is your name? Enter: ")
 guess = int(input("Guess a number between 1 and " + str(difficulty) + ":"))
