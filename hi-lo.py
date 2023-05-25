17b53d3037fdea97ccc96a5c7aa76540f02285ab
Pixlox <85137818+Pixlox@users.noreply.github.com>
2023-05-25 20:10:02 +1000

    added required challenges to hi-lo game

--- hi-lo.py
+++ hi-lo.py
@@ -1,18 +1,34 @@
 import random
 
-secret = random.randint(1,10)
+def grabDifficulty():
+    difficultyWord = input("Choose a difficulty (easy / medium / hard) ")
+
+    if difficultyWord == "easy":
+        return 10
+    elif difficultyWord == "medium":
+        return 20
+    elif difficultyWord == "hard":
+        return 30
+
+difficulty = grabDifficulty()
+secret = random.randint(1, difficulty)
 count = 0
 
-guess = int(input("Guess a number between 1 and 10: "))
+name = input("What is your name? Enter: ")
+guess = int(input("Guess a number between 1 and " + str(difficulty) + ":"))
 print(guess)
 
 while guess != secret:
     if guess > secret:
-        print("LOWER")
+        count = count + 1
+        print("Lower...")
     
     elif guess < secret:
-        print("HIGHER")
+        count = count + 1
+        print("Higher...")
 
-    guess = int(input("Guess again: "))  
+    guess = int(input("Guess again: "))
 
-print("CORRECT")
+    if guess == secret:
+        print("Correct!")
+        print("Guessing took you " + str(count) + " tries, " + name + "!")
